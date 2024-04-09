29731dad9104e4a94e8f73e09768027ff12634d0
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2024-04-09 15:10:36 -0300

    Delete aula_login2_sessao directory

--- aula_login2_sessao/menu.php
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<br><br><br><br><br><br><br><br><br><br>
-<font size=7 color=red> Entrei <?php require('verifica.php'); echo $_SESSION["nome_usuario"]; ?></font>
-<br><br><br> <a href="logout.php"> sair </a>
-</table>
