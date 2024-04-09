c267d2c241c32cbe39cc6cca09f3ad841884fe53
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2024-04-09 15:10:26 -0300

    Delete aula_login1_simples directory

--- aula_login1_simples/logout.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "<script>alert('Você saiu!');top.location.href='login.php';</script>"; 
-?>
--- aula_login1_simples/menu.php
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<br><br><br><br><br><br><br><br><br><br>
-<font size=7 color=red> Entrei </font>
-<br><br><br> <a href="logout.php"> sair </a>
-</table>
